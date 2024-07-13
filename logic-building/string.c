1c203fc1c83514a89ef6b70dfc7161ed1314bf8d
brijeshpytops <brijeshgondaliya.tops@example.com>
2024-07-13 14:03:43 +0530

    string

---
+++ logic-building/string.c
@@ -0,0 +1,19 @@
+TASK-1
+----------------------------------------------------------------
+step-1: Enter a string : "PYtHoN CoDE"
+
+step-2:
+Choose options:
+lower PRESS-1 - python code
+upper PRESS-2 - PYTHON CODE
+swap  PRESS-3 - pyThOn cOde
+title PRESS-4 - Python Code
+Capitalize PRESS-5 - Python code
+
+TASK-2
+----------------------------------------------------------------
+Find the string length manually using c function
+
+step-1: Enter a string : "PYtHoN CoDE"
+
+
