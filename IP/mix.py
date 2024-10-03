c86606364e91643397870efeb975cae63a7b11c2
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-10-03 09:13:59 +0530

    IP

---
+++ IP/mix.py
@@ -0,0 +1,62 @@
+# code  = "Python"
+
+# print(len(code)) # Output: 6
+
+# def length(c):
+#     count = 0
+#     for ch in c:
+#         count += 1
+#     return count
+# print(length(code))
+
+# print([1,2,3].append(4))
+
+# nums = [1,2,3]
+# nums.append(4)
+# print(nums)
+
+
+# nums = [45,66, 265, 968, 2, 4058, 476]
+
+# for i in range(1, 5):
+#     for index, num in enumerate(nums):
+#         if index < len(nums)-1:
+#             if nums[index] > nums[index+1]:
+#                 nums[index], nums[index+1] = nums[index+1], nums[index]
+# print(nums[-2])
+
+# max_number = nums[0]
+# for num in nums:
+#     if num > max_number:
+#         max_number = num
+# print(max_number)
+
+# print(max(nums))
+# print(min(nums))
+# print(sum(nums)/len(nums))
+
+
+
+nums = [1,2,3,[4,5,6],7,8,9]
+
+# flag = False
+# for num in nums:
+#     if num == [4,5,6]:
+#         flag = True
+#         break
+# if(flag):
+#     print("Found")
+# else:
+#     print("Not Found")
+
+
+# flag = False
+# for num in nums:
+#     if isinstance(num, list):
+#         flag = True
+#         break
+# if(flag):
+#     print("Found")
+# else:
+#     print("Not Found")
+
