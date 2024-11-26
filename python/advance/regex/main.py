a7b877db846ea3a6378e3b6cfbcc3fff77c2661c
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-11-26 10:24:13 +0530

    regex

---
+++ python/advance/regex/main.py
@@ -0,0 +1,18 @@
+import re
+
+# data = "dkjgfkahj niharnakrani07@gmail.com ,jsaud ,mas,las dlkj ,a,hs 12-23-1995 kj hkn,uy uygldkn brijesh.gondaliya07@gmail.com sdmuh;.k mkijuhnd; . apple kjsgdlkjcnlk brijeshgondaliya.tops@gmail.com jdfhskjd .lkjh;loijd hjgkfkus,layugs l gl;idfh zsiuuyh"
+
+# date_patterns = r'\b\d{1,2}[-/]\d{1,2}[-/]\d{4}\b'
+# email_patterns = r'\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}\b'
+# # res = re.findall(email_patterns, data)
+# # res = re.match(date_patterns, data)
+# # res = re.search('apple', data)
+# res = re.split('\s', data)
+
+# for d in res:
+#     if d.endswith('.com'):
+#         print(f"Email: {d}")
+
+# data = "Kumbhmela conduct in rajkot"
+# res = re.sub('rajkot', 'junagadh', data)
+# print(res)
